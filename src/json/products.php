e81be97d208f63962e5aa965a3a262f76fa7f029
fg716463 <fg716463@784e779e-19d6-4006-af7c-3b0eff316831>
2014-07-21 07:24:48 +0000

    Fixed bug. Products can now be deleted
    git-svn-id: https://svn.rwth-aachen.de/repos/da026566_hpc/projects/ITC-Kaffestrichliste@5334 784e779e-19d6-4006-af7c-3b0eff316831

--- src/json/products.php
+++ src/json/products.php
@@ -7,7 +7,7 @@
 				$now = new DateTime();
 				$time = mktime(0, 0, 0, $now->format("m"), 1, $now->format("Y"));
 				$user = $this->coffee->getUser();
-				$query = $this->coffee->db()->prepare("SELECT p.price AS price, p.name AS name, p.id AS id, COUNT(t.id) AS amount FROM Products p LEFT JOIN Transactions t ON t.product = p.id AND t.user = ? AND t.date >= ? GROUP BY p.id");
+				$query = $this->coffee->db()->prepare("SELECT p.price AS price, p.name AS name, p.id AS id, COUNT(t.id) AS amount FROM Products p LEFT JOIN Transactions t ON t.product = p.id AND t.user = ? AND t.date >= ? WHERE p.deleted = FALSE GROUP BY p.id");
 				$query->bind_param("ii", $user, $time);
 				$query->execute();
 				$query->bind_result($price, $name, $id, $amount);
